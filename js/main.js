03299e2e566b06fef52d79b7381e3fb1af9120cc
guiyee <guillermodinanno@gmail.com>
2023-05-04 14:47:03 -0300

    starting with contact form JS

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const projectImg = document.querySelector('.blur-img');
-projectImg.addEventListener('mouseover', function() {
-  this.style.backgroundColor = 'hsla(0, 0%, 0%, 0.75)';
-});
-
-projectImg.addEventListener('mouseout', function() {
-  this.style.backgroundColor = 'transparent';
-});
